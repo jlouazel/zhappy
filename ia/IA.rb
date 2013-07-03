63cb9a04f8b88604477df872f373340920acada0
Manyuuz <M.Manoury@gmail.com>
2013-07-03 16:47:29 +0200

    Decoupage ia ruby et perl pour avoir des classes propres

--- ia/IA.rb
+++ ia/IA.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 require 'socket'
-require 'optparse'
 
 class IA
 	attr_reader :team
@@ -12,38 +11,4 @@ class IA
 		@team = team
 		@socket = TCPSocket.new host, port
 	end
-end
-
-class OptparseArgs
-	def self.parse(args)
-		options = {}
-		opt_parser = OptionParser.new do |opts|
-			opts.on('-n TEAM') do |team|
-        		options[:team] = team
-        	end
-        	opts.on('-h HOST') do |host|
-        		options[:host] = host
-        	end
-        	opts.on('-p PORT') do |port|
-        		options[:port] = Integer(port)
-        	end
-        end
-    opt_parser.parse!(args)
-    options
-    end
-end
-
-begin
-	if ARGV.size < 6 then
-		raise ArgumentError, 'Not enought arguments provided'
-	end
-	if ARGV.size > 6 then
-		raise ArgumentError, 'Too many arguments provided'
-	end
-	options = OptparseArgs.parse(ARGV)
-	printf("Team : %s, Host : %s, Port : %d\n", options[:team], options[:host], options[:port])
-	bot = IA.new(options[:team], options[:host], options[:port])
-rescue => e
-	puts "An error occured (" + e + ")"
-	puts "Usage: ./IA.rb [-n team_name] [-h hostname] [-p port]"
 end
