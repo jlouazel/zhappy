06ac189d4de6c4f243bf0210e7fb755131c2d76e
Manyuuz <M.Manoury@gmail.com>
2013-07-06 15:46:07 +0200

    old modifs ia manu

--- IA/main.rb
+++ IA/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'IA'
 
 class OptparseArgs
 	def self.parse(args)
@@ -21,16 +22,31 @@ class OptparseArgs
     end
 end
 
+# "try" facon ruby
 begin
+	# gestion du nombre d'arguments
 	if ARGV.size < 6 then
 		raise ArgumentError, 'Not enought arguments provided'
 	end
 	if ARGV.size > 6 then
 		raise ArgumentError, 'Too many arguments provided'
 	end
+	
+	# parsing des arguments
 	options = OptparseArgs.parse(ARGV)
-	printf("Team : %s, Host : %s, Port : %d\n", options[:team], options[:host], options[:port])
+	
+	# instantiation d'une IA
 	bot = IA.new(options[:team], options[:host], options[:port])
+	
+	# boucle pour la socket client 
+	connected = true
+	while connected == true do
+		bot.socket.puts("Keukou")
+		connected = false
+	end
+	puts "You have been disconnected"
+	
+# "catch" facon ruby
 rescue => e
 	puts "An error occured (" + e + ")"
 	puts "Usage: ./IA.rb [-n team_name] [-h hostname] [-p port]"
--- ia/IA.pm
+++ ia/IA.pm
@@ -13,4 +13,26 @@ sub new {
  $this->{socket}->autoflush(1);
  return $this;
 }
+
+sub authenticate {
+	my ($this) = @_;
+	my $buf = "";
+	my @array = ();
+	# reception du "BONJOUR\n"
+	$this->{socket}->recv($buf, 2048);
+	sleep(0.05);
+	# envoi du nom d'equipe
+	$this->{socket}->send($this->{teamname}."\n");
+	sleep(0.05);
+	# reception de l'id
+	$this->{socket}->recv($buf, 2048);
+	$this->{id} = substr($buf, 0, -1);
+	sleep(0.05);
+	# reception de la taille de la map
+	$this->{socket}->recv($buf, 2048);
+	@array = split(/ /, substr($buf, 0, -1));
+	$this->{max_x} = @array[0];
+	$this->{max_y} = @array[1];
+	print "Je suis le ".$this->{id}."eme joueur de l'equipe ".$this->{teamname}." et le monde fait ".$this->{max_x}." x ".$this->{max_y}."\n";
+}
 1;
