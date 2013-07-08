58051d0508754b9cfc4530619caa9c1d21a612ab
Manyuuz <M.Manoury@gmail.com>
2013-07-08 15:19:35 +0200

    conditions elevation

--- IA/player.py
+++ IA/player.py
@@ -487,28 +487,26 @@ class player:
     def incantIfPossible(self):
         myNeed = self._elevation.getNeed(self._lvl)
         i = self._lenMapX * self._posY + self._posX
-        # si y'a assez ou plus de pierres pour l'incantation
-        if (self._inventaire._linemate + self._map[i]._linemate >= myNeed._linemate and self._inventaire._deraumere + self._map[i]._deraumere >= myNeed._deraumere and self._inventaire._sibur + self._map[i]._sibur >= myNeed._sibur and self._inventaire._mendiane + self._map[i]._mendiane >= myNeed._mendiane and self._inventaire._phiras + self._map[i]._phiras >= myNeed._phiras and self._inventaire._thystame + self._map[i]._thystame >= myNeed._thystame and self._lvl == 1):
-        	# si y'a pile ce qu'il faut
-            if (self._map[i]._linemate == myNeed._linemate and self._map[i]._deraumere == myNeed._deraumere and self._map[i]._sibur == myNeed._sibur and self._map[i]._mendiane == myNeed._mendiane and self._map[i]._phiras == myNeed._phiras and self._map[i]._thystame == myNeed._thystame):
+        # si y'a assez ou plus de pierres sur la map + inventaire pour l'incantation
+        if (self._inventaire._linemate + self._map[i]._linemate >= myNeed._linemate and self._inventaire._deraumere + self._map[i]._deraumere >= myNeed._deraumere and self._inventaire._sibur + self._map[i]._sibur >= myNeed._sibur and self._inventaire._mendiane + self._map[i]._mendiane >= myNeed._mendiane and self._inventaire._phiras + self._map[i]._phiras >= myNeed._phiras and self._inventaire._thystame + self._map[i]._thystame >= myNeed._thystame):
+        	# si y'a pile ce qu'il faut par terre
+            if (self._map[i]._linemate == myNeed._linemate and self._map[i]._deraumere == myNeed._deraumere and self._map[i]._sibur == myNeed._sibur and self._map[i]._mendiane == myNeed._mendiane and self._map[i]._phiras == myNeed._phiras and self._map[i]._thystame == myNeed._thystame and self._map[i]._players >= myNeed._joueur):
                 self.incantation()
-                #print "elevation"
                 return False
-            # sinon si y'en a pas assez
+            # sinon si y'en a pas assez par terre
             elif (self._map[i]._linemate < myNeed._linemate or self._map[i]._deraumere < myNeed._deraumere or self._map[i]._sibur < myNeed._sibur or self._map[i]._mendiane < myNeed._mendiane or self._map[i]._phiras < myNeed._phiras or self._map[i]._thystame < myNeed._thystame):
-                #if Il manque des objects :
-                #print "I can't move."
                 self.putObjectIncantation()
                 self.voir()
                 return True
-            # sinon si y'en a trop
+            # sinon si y'en a trop par terre
             elif (self._map[i]._linemate > myNeed._linemate or self._map[i]._deraumere > myNeed._deraumere or self._map[i]._sibur > myNeed._sibur or self._map[i]._mendiane > myNeed._mendiane or self._map[i]._phiras > myNeed._phiras or self._map[i]._thystame > myNeed._thystame):
-                #print "I can't move."
                 self.takeObjectIncantation()
                 self.voir()
                 return True
-            #elif Il manque des personnes brodcast Incantation
-            
+            # si y'a pas assez de joueurs
+            elif (self._map[i]._players < myNeed._joueur):
+            	print "Pas assez de joueurs -> broadcast"
+            	return True
         return False
 
     def findGoodMove(self):
