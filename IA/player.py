93565c62dffddb2d1f3075487307aeb897851fdd
Manyuuz <M.Manoury@gmail.com>
2013-07-10 14:47:17 +0200

    Recuperation de X et Y dans la reception du "j'suis arrive"

--- IA/player.py
+++ IA/player.py
@@ -381,7 +381,7 @@ class player:
         elif (self._action._firstAction == self._action._PossibleAction._incantation):
             print "Incantation amie je suis arive sur place ", self._posX, " ", self._posY
             self._action._define = 0
-            self.broadcast("A"+str(self._posX)+"/"+str(self._posY))
+            self.broadcast("A:"+str(self._posX)+"/"+str(self._posY))
 
     def takeObject(self):
         i = self._lenMapX * self._posY + self._posX
@@ -634,7 +634,8 @@ class player:
             elif msg[0:9] == "Incomming":
             	self._incomming += 1
             	print "Il y a " + str(self._incomming) + " personnes qui viennent m'aider a evoluer"
-            elif msg[0:1] == "A":
-            	coordX = msg[1:].split('/')[0]
-            	coordY = msg[1:].split('/')[1]
+            elif msg[0:1] == "A:":
+            	msg = msg.split(':')[1]
+            	coordX = msg.split('/')[0]
+            	coordY = msg.split('/')[1]
 
