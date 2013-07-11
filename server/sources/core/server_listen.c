86c41563064e61740b4b8c2af557b93d85e3400d
Julien Fortin <julien.fortin@epitech.eu>
2013-07-11 10:47:49 +0200

    ko verif team

--- server/sources/core/server_listen.c
+++ server/sources/core/server_listen.c
@@ -5,16 +5,13 @@
 ** Login   <fortin_j@epitech.net>
 **
 ** Started on  Mon Jul  1 10:09:16 2013 julien fortin
-** Last update Wed Jul 10 22:27:55 2013 julien fortin
+** Last update Thu Jul 11 10:46:12 2013 julien fortin
 */
 
 #include	<stdlib.h>
 #include	<sys/select.h>
-#include	"lib_strings.h"
 #include	"server.h"
 #include	"player.h"
-
-#include	<stdio.h>
 
 bool		server_listen_player(const t_server *serv, fd_set *rfd, int *max_fd)
 {
@@ -32,24 +29,5 @@ bool		server_listen_player(const t_server *serv, fd_set *rfd, int *max_fd)
 	}
       clients = clients->next;
     }
-  return (true);
-}
-
-bool		server_get_auth_from_player(const t_server *serv,
-					    t_player *player,
-					    const char *data)
-{
-  t_list	*list;
-
-  if (!my_strcmp("GRAPHIC", data))
-    {}
-  else if ((list = serv && serv->game && serv->game->teams ? serv->game->teams : NULL))
-    while (list)
-      {
-	if (list->data && !my_strcmp(((t_team*)list->data)->name, data))
-	  return (((t_team*)list->data)->add_player(((t_team*)list->data), serv, player));
-	list = list->next;
-      }
-  //acepter les mecs et voir la diff entre client ia/graphic
   return (true);
 }
