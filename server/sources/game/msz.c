b44c5bc63144ecaa960d09be9455693ae6b706e2
Julien Fortin <julien.fortin@epitech.eu>
2013-07-13 23:12:51 +0200

    ca compile plus mieux toujours

--- server/sources/game/msz.c
+++ server/sources/game/msz.c
@@ -5,7 +5,7 @@
 ** Login   <louaze_j@epitech.net>
 **
 ** Started on  Mon Jul  8 15:09:51 2013 louaze_j
-** Last update Sat Jul 13 22:37:32 2013 julien fortin
+** Last update Sat Jul 13 23:12:41 2013 julien fortin
 */
 
 #include	"server.h"
@@ -13,9 +13,15 @@
 
 #include	<stdio.h>
 
-const char	*_graph_msz(t_graphical *graphical, const t_server *server)
+static const char	*_graph_msz(t_graphical *graphical, const t_server *server)
 {
   (void)graphical;
   printf("msz %d %d\n", server->game->world->width, server->game->world->height);
   return (NULL);
+}
+
+void	_msz(t_graphical *graph)
+{
+  if (graph)
+    graph->msz = &_graph_msz;
 }
