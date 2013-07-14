e2ecf28e21b4db97253ab901012a3db7501ae2dc
moreaup <alexandre.moreau@epitech.eu>
2013-07-14 23:38:20 +0200

    s

--- IA.sh
+++ IA.sh
@@ -4,8 +4,8 @@ if [ $# = 3 ]
 then
     for (( i=0; i < 12; i++ ))
     do
-	./IA/main.py -n $1 -h $2 -p $3
-	sleep 5s
+	./IA/main.py -n $1 -h $2 -p $3 &
+	sleep 7s
     done
 else
     echo "Usage : ./IA.sh [TEAM] [HOST] [PORT]"
