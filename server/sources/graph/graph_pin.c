6f4a64790313d2d9c6879bc0304b6bba63635430
Julien Fortin <julien.fortin@epitech.eu>
2013-07-14 10:03:40 +0200

    pin

--- server/sources/graph/graph_pin.c
+++ server/sources/graph/graph_pin.c
@@ -5,7 +5,7 @@
 ** Login   <fortin_j@epitech.net>
 **
 ** Started on  Sun Jul 14 00:31:59 2013 julien fortin
-** Last update Sun Jul 14 10:00:24 2013 julien fortin
+** Last update Sun Jul 14 10:03:12 2013 julien fortin
 */
 
 #include	"graphical.h"
@@ -15,10 +15,7 @@ const char	*_graph_pin(t_graphical *graph,
 			    const t_server *server,
 			    const char *data)
 {
-  int	id;
-
   if (!graph || !server || !data || !data[0])
     return ("ko\n");
-  get_player_for_id(server, atoi(data));
-  return ("ko\n");
+  return (graph->pin(graph, server, )get_player_for_id(server, atoi(data)));
 }
